cf83cf9ba2282c3d542396cf729a365b64c020e1
schattian <sebachamena@gmail.com>
2021-10-24 21:42:49 -0300

    Cleanup vm pkg

--- compiler/vm/const.go
+++ compiler/vm/const.go
@@ -1,5 +1,29 @@
 package vm
 
+type operation string
+
+const (
+	OpPush     operation = "push"
+	OpPop      operation = "pop"
+	OpAdd      operation = "add"
+	OpSub      operation = "sub"
+	OpNeg      operation = "neg"
+	OpEq       operation = "eq"
+	OpGt       operation = "gt"
+	OpLt       operation = "lt"
+	OpAnd      operation = "and"
+	OpOr       operation = "or"
+	OpNot      operation = "not"
+	OpFunction operation = "function"
+	OpCall     operation = "call"
+	OpReturn   operation = "return"
+	OpLabel    operation = "label"
+	OpGoto     operation = "goto"
+	OpIfGoto   operation = "if-goto"
+)
+
+type memSegment string
+
 const (
 	SegLcl     memSegment = "local"
 	SegArg     memSegment = "argument"
@@ -9,31 +33,4 @@ const (
 	SegConst   memSegment = "constant"
 	SegThis    memSegment = "this"
 	SegThat    memSegment = "that"
-
-	OpPush operation = "push"
-	OpPop  operation = "pop"
-
-	OpAdd operation = "add"
-	OpSub operation = "sub"
-	OpNeg operation = "neg"
-	OpEq  operation = "eq"
-	OpGt  operation = "gt"
-	OpLt  operation = "lt"
-	OpAnd operation = "and"
-	OpOr  operation = "or"
-	OpNot operation = "not"
-
-	OpFunction operation = "function"
-	OpCall     operation = "call"
-	OpReturn   operation = "return"
-
-	OpLabel  operation = "label"
-	OpGoto   operation = "goto"
-	OpIfGoto operation = "if-goto"
-
-	initFunc string = "Sys.init"
 )
-
-type operation string
-
-type memSegment string
